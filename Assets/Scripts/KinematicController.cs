e71c36b0fb1b39c5c49474bd46085a9deae231ca
Jacob Leonardo <leonardojacob257@gmail.com>
2024-03-27 11:55:48 -0600

    3/27

--- Assets/Scripts/KinematicController.cs
+++ Assets/Scripts/KinematicController.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class KinematicController : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
+    [SerializeField] Space space = Space.World;
 
     void Update()
     {
         Vector3 direction = Vector3.zero;
+        float rotation = 0;
 
-        direction.x = Input.GetAxisRaw("Horizontal");
+        if(space == Space.World) direction.x = Input.GetAxisRaw("Horizontal");
+        else
+        {
+            rotation = Input.GetAxis("Horizontal");
+        }
         direction.z = Input.GetAxisRaw("Vertical");
+        direction = Vector3.ClampMagnitude(direction, 1);
 
-        Vector3.ClampMagnitude(direction, 1);
+        transform.rotation *= Quaternion.Euler(0, rotation * speed, 0);
+        //direction = transform.rotation * direction;
 
-        transform.position += direction * speed * Time.deltaTime;
+        transform.Translate(direction * speed * Time.deltaTime, space);
+        //transform.position += direction * speed * Time.deltaTime;
     }
 }
--- Assets/Scripts/PhysicsCollider.cs
+++ Assets/Scripts/PhysicsCollider.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class PhysicsCollider : MonoBehaviour
 {
 	string status = "";
+	Vector3 contact;
+	Vector3 normal;
+	[SerializeField] private GameObject explosion;
 
 	private void OnCollisionEnter(Collision collision)
 	{
 		status = "collision enter: " + collision.gameObject.name;
+
+		contact = collision.GetContact(0).point;
+		normal = collision.GetContact(0).normal;
+
+		Instantiate(explosion, contact, Quaternion.LookRotation(normal));
 	}
 
 	private void OnCollisionStay(Collision collision)
 	{
-		
+		status = "collision stay: " + collision.gameObject.name;
 	}
 
 	private void OnCollisionExit(Collision collision)
 	{
-		
+		status = "collision exit: " + collision.gameObject.name;
 	}
 
 	private void OnTriggerEnter(Collider other)
@@ -28,12 +36,12 @@ public class PhysicsCollider : MonoBehaviour
 
 	private void OnTriggerStay(Collider other)
 	{
-		
+		status = "collision stay: " + other.gameObject.name;
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		
+		status = "collision exit: " + other.gameObject.name;
 	}
 
 	private void OnGUI()
@@ -41,5 +49,12 @@ public class PhysicsCollider : MonoBehaviour
 		GUI.skin.label.fontSize = 24;
 		Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
 		GUI.Label(new Rect(screen.x, Screen.height - screen.y, 250, 70), status);
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(contact, 0.1f);
+		Gizmos.DrawLine(contact, contact + normal);
 	}
 }

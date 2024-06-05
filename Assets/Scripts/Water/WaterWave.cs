df04f88f92da77e1ff522af26663ea67cb778690
Jacob Leonardo <leonardojacob257@gmail.com>
2024-06-04 18:13:53 -0700

    water

--- Assets/Scripts/Water/WaterWave.cs
+++ Assets/Scripts/Water/WaterWave.cs
@@ -10,12 +10,13 @@ public class WaterWave : MonoBehaviour
 	struct Wave
 	{
 		[Range(0, 10)] public float amplitude;
-		[Range(0, 10)] public float length;
+		[Range(0, 40)] public float length;
 		[Range(0, 2)] public float roll;
 		[Range(0, 10)] public float rate;
+		[Range(0, 360)] public float direction;
 	}
 
-	[SerializeField] Wave wave;
+	[SerializeField] Wave[] waves;
 
 	[Header("Mesh Generator")]
 	[SerializeField][Range(1, 80)] float xSize = 40;
@@ -43,12 +44,16 @@ public class WaterWave : MonoBehaviour
 		buffer = new Vector3[xVertexNum, zVertexNum];
 	}
 
-	Vector3 GerstnerWave(Vector3 position, float time, float length, float amplitude, float roll)
+	Vector3 GerstnerWave(Vector3 position, Vector2 direction, float speed, float length, float amplitude, float roll)
 	{
 		Vector3 v = Vector3.zero;
 
-		v.x = Mathf.Cos(position.x * length + time) * roll;
-		v.y = Mathf.Sin(position.x * length + time) * amplitude;
+		float coord = position.x * direction.x + position.z * direction.y;
+		float k = 2 * Mathf.PI / length;
+		float f = k * coord + speed;
+
+		v.x = Mathf.Cos(f) * roll;
+		v.y = Mathf.Sin(f) * amplitude;
 
 		return v;
 	}
@@ -66,12 +71,20 @@ public class WaterWave : MonoBehaviour
 				p.z = ((z / (float)zVertexNum - 1) - 0.5f) * zSize;
 				//p.y = Mathf.Sin((p.x * wave.length) + Time.time * wave.rate) * wave.amplitude; 
 
-				Vector3 o = GerstnerWave(p, Time.time * wave.rate, wave.length, wave.amplitude, wave.roll);
+				Vector3 o = Vector3.zero;
+				for (int i = 0; i < waves.Length; i++)
+				{
+					Vector2 d = new Vector2(Mathf.Cos(Mathf.Deg2Rad * waves[i].direction), Mathf.Sin(Mathf.Deg2Rad * waves[i].direction));
+					d.Normalize();
 
+					o += GerstnerWave(p, d, Time.time * waves[i].rate, waves[i].length, waves[i].amplitude, waves[i].roll);
+				}
 
 				buffer[x, z] = p + o;
 			}
 		}
+
+
 
 		// set vertices from buffer
 		for (int x = 0; x < xVertexNum; x++)
